444e04b7e6a139636706e5fd8f1b88278aebd5d3
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-12-01 14:13:48 +0300

    Feat file to export all images as an array

---
+++ src/imageArr.js
@@ -0,0 +1,27 @@
+import img1 from "src/assets/haberdoedas-HFv0OedrSVk-unsplash.jpg";
+import img2 from "src/assets/haberdoedas-jlePphcHBOo-unsplash.jpg";
+import img3 from "src/assets/kelly-sikkema-XHXOafBIOhc-unsplash.jpg";
+import img4 from "src/assets/nik-5dgHxsvJ170-unsplash.jpg";
+import img5 from "src/assets/rocco-dipoppa-Nh4rV0ParHw-unsplash.jpg";
+import img6 from "src/assets/valentino-montironi-0rE0j1Vhvzo-unsplash.jpg";
+import img7 from "src/assets/valentino-montironi-upcYBQvK0tY-unsplash.jpg";
+import img8 from "src/assets/yazid-n--Puj9KSnvJ8-unsplash.jpg";
+import img9 from "src/assets/yazid-n-iLFCwPQWCbM-unsplash.jpg";
+import img10 from "src/assets/yazid-n-pbt5I-C6ig0-unsplash.jpg";
+import img11 from "src/assets/yulia-matvienko-ArA3S3k0wTU-unsplash.jpg";
+import img12 from "src/assets/yulia-matvienko-kgz9vsP5JCU-unsplash.jpg";
+
+export default imageArr = [
+	img1,
+	img2,
+	img3,
+	img4,
+	img5,
+	img6,
+	img7,
+	img8,
+	img9,
+	img10,
+	img11,
+	img12,
+];
