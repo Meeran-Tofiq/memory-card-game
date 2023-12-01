f5d7664eb31c81925edefae50c0c258fc7e5f3ef
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2023-12-01 17:41:35 +0300

    Add file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+	return (
+		<footer>
+			Made by{" "}
+			<a href="https://github.com/Meeran-Tofiq">
+				Meeran Tofiq <i class="fa-brands fa-github" />
+			</a>
+		</footer>
+	);
+}
